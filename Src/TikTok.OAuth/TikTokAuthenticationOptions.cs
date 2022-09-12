e508bcd4babafdc61b167c5aaa85ea41bc6f9c84
stormbringer <dantedmc78@gmail.com>
2022-09-12 08:41:33 +0300

    Update: TikTok.OAuth - moved auth defaults && options here

---
+++ Src/TikTok.OAuth/TikTokAuthenticationOptions.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Authentication.OAuth;
+
+namespace TikTok.OAuth;
+
+public class TikTokAuthenticationOptions : OAuthOptions
+{
+    public TikTokAuthenticationOptions()
+    {
+        AuthorizationEndpoint = TikTokAuthenticationDefaults.AuthorizationEndpoint;
+        TokenEndpoint = TikTokAuthenticationDefaults.TokenEndpoint;
+        // UserInformationEndpoint = TikTokAuthenticationDefaults.UserInformationEndpoint;
+        // CallbackPath = new PathString("/auth/signin-tiktok");
+        CallbackPath = TikTokAuthenticationDefaults.CallbackPath;
+        Scope.Add("user.info.basic");
+        Scope.Add("video.list");
+        Scope.Add("video.upload");
+    }
+}
